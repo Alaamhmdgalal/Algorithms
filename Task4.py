bc7783d6d41daaf98b1196d26286992d008f1224
Alaamhmdgalal <alaaemran99@gmail.com>
2022-04-28 01:29:58 +0200

    Penny Distribution

--- Task4.py
+++ Task4.py
@@ -6,14 +6,7 @@ box = [0] * int((math.log(n)/math.log(2))+1)
 box[0] = n
 print(box)
 for i in range (0, len(box)):
-    while(box[i] !=0 and box[i] > 0 and box[i] != 1):
-        if (n % 2 == 0):
-            box[i] -=2
-            box[i+1] +=1
-            print(box)
-            n-=2
-
-        else:
+    while(box[i] !=0 and box[i] != 1):
             box[i] -=2
             box[i+1] +=1
             print(box)
